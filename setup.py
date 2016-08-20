e2f991b9f6ca3c0ac9fdc365caa6ac768c478253
小倉大樹 <dogura@oguradaiki-no-MacBook-Pro.local>
2016-08-20 16:00:51 +0900

    init repo

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+install_requires = open('requirements.txt').read().split('\n')
+readme_content = open('README.md').read()
+
+
+setup(
+    name='py-json-rpc',
+    version='0.0.1',
+    description='Decorator based toolkit to use JSONRPC easy like Flask',
+    long_description=readme_content,
+    author='OGURA_Daiki',
+    author_email='',
+    license='MIT',
+    keywords=['json rpc', 'smart home'],
+    url='https://github.com/hachibeeDI/py-json-rpc',
+    packages=find_packages(),
+    scripts=['bin/json_rpc'],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
